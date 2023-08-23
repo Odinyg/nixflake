e88df02612d27dc30954954a6c1d5737e978ad80
odin <odin@nygard.io>
2023-08-23 23:53:43 +0200

    refactor nixvim

--- home/features/cli/neovim/default.nix
+++ home/features/cli/neovim/default.nix
@@ -1,6 +1,28 @@
 {
   imports = [
-    ./nixvim.nix
+ #    ./autocommands.nix
+ #   ./completion.nix
+ #   ./keymappings.nix
+    ./options.nix
+ #   ./plugins
+ #   ./todo.nix
   ];
 
+  home = {
+    shellAliases.v = "nvim";
+
+    sessionVariables.EDITOR = "nvim";
+  };
+
+  programs.nixvim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+
+    luaLoader.enable = true;
+
+    # Highlight and remove extra white spaces
+    highlight.ExtraWhitespace.bg = "red";
+    match.ExtraWhitespace = "\\s\\+$";
+  };
 }
