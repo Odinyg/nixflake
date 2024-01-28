f538c66ec1ecbf53cd94f85e874f7c6e7a4aed9f
Odin <git@pytt.io>
2024-01-28 22:05:25 +0100

    moved everything to modules

--- home/p53.nix
+++ home/p53.nix
@@ -1,14 +1,14 @@
-{ inputs, outputs, ... }: {
+{ ... }: {
   imports = [
-    ./features/cli
-    ./features/common
+#j    ./features/cli
+#    ./features/common
   ];
 nixpkgs.config.allowUnfree = true;
 
   home = {
     username = "odin";
     homeDirectory = "/home/odin";
-    stateVersion = "23.11";
+    stateVersion = "24.05";
   };
 programs.home-manager.enable = true;
 }
