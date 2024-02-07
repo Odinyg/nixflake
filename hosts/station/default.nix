0ffd89fd4d146ddfe7d51e47375ca0d9f35c1b2f
Odin <git@pytt.io>
2024-02-07 16:53:55 +0100

    remove comment

--- hosts/station/default.nix
+++ hosts/station/default.nix
@@ -68,7 +68,7 @@
     };
   services.openssh.enable = true;
 
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.05"; 
 
   hardware.opengl = {
     enable = true;
