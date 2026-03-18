0fa6608537794bb669d3f4256f27c21041d4b1ba
Odin <git@pytt.io>
2026-03-18 20:22:21 +0100

    alloy: configure journal log shipping to loki, improve loki dashboard

--- modules/server/monitoring.nix
+++ modules/server/monitoring.nix
@@ -1,5 +1,12 @@
-{ ... }:
-
+{
+  config,
+  lib,
+  ...
+}:
+let
+  lokiUrl = "http://10.10.30.112:3100/loki/api/v1/push";
+  hostname = config.networking.hostName;
+in
 {
   # Grafana Alloy for log + metric collection on every server
   services.alloy.enable = true;
@@ -8,6 +15,22 @@
   systemd.services.alloy.serviceConfig = {
     SupplementaryGroups = [ "systemd-journal" ];
   };
+
+  environment.etc."alloy/config.alloy".text = ''
+    loki.source.journal "journal" {
+      forward_to = [loki.write.default.receiver]
+      labels = {
+        host = "${hostname}",
+        job  = "journal",
+      }
+    }
+
+    loki.write "default" {
+      endpoint {
+        url = "${lokiUrl}"
+      }
+    }
+  '';
 
   # Prometheus node exporter for system metrics
   services.prometheus.exporters.node = {
