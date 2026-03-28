eda7ed6806bc363e1d306f43e14155f336ad1f96
Odin <git@pytt.io>
2026-03-28 15:00:34 +0100

    ollama unstable

--- modules/nixos/ollama.nix
+++ modules/nixos/ollama.nix
@@ -1,10 +1,11 @@
-{ lib, config, ... }: {
+{ lib, config, pkgs-unstable, ... }: {
 
   options = { ollama = { enable = lib.mkEnableOption "ollama"; }; };
   config = lib.mkIf config.ollama.enable {
     networking.extraHosts = "127.0.0.1 ollama.local";
     services.ollama = {
       enable = true;
+      package = pkgs-unstable.ollama;
       host = "0.0.0.0";
       acceleration = "cuda";
       openFirewall = true;
