006b38224f611fe1a1bbf0ac10781f7d5fde130d
Odin <git@pytt.io>
2026-04-03 14:00:42 +0200

    cleanup: standardize home-manager module patterns
    - discord.nix: convert from outer 'config = lib.mkIf' to standard 'config.home-manager.users = lib.mkIf' pattern (matches 26 other HM modules)
    - lmstudio.nix: reformat inline options to multi-line for readability
    
    Note: thunar.nix and zen-browser.nix intentionally use the outer pattern because they mix system-level config (environment.systemPackages) with home-manager config.

--- modules/home-manager/app/discord.nix
+++ modules/home-manager/app/discord.nix
@@ -11,21 +11,19 @@
     };
   };
 
-  config = lib.mkIf config.discord.enable {
-    home-manager.users.${config.user} = {
-      home.packages = with pkgs; [
-        vesktop
-      ];
-      xdg.configFile."discord/settings.json".text = ''
-        {
-          "BACKGROUND_COLOR": "#162e52",
-          "IS_MAXIMIZED": false,
-          "IS_MINIMIZED": false,
-          "OPEN_ON_STARTUP": false,
-          "MINIMIZE_TO_TRAY": false,
-          "SKIP_HOST_UPDATE": true
-        }
-      '';
-    };
+  config.home-manager.users.${config.user} = lib.mkIf config.discord.enable {
+    home.packages = with pkgs; [
+      vesktop
+    ];
+    xdg.configFile."discord/settings.json".text = ''
+      {
+        "BACKGROUND_COLOR": "#162e52",
+        "IS_MAXIMIZED": false,
+        "IS_MINIMIZED": false,
+        "OPEN_ON_STARTUP": false,
+        "MINIMIZE_TO_TRAY": false,
+        "SKIP_HOST_UPDATE": true
+      }
+    '';
   };
 }
